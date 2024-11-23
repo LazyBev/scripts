cc7e63d7e3b770abeef1be61477090f21258d5d2
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2024-11-23 05:42:51 +0000

    Update bluetooth.sh

--- bluetooth.sh
+++ bluetooth.sh
@@ -9,16 +9,16 @@ help=false
 # Parse arguments
 for arg in "$@"; do
     case $arg in
-		-r | --root)
-  			root=true
+	-r | --root)
+  	    root=true
             ;;
-	    -h | --help)
+	-h | --help)
             help=true
             ;;
-	    *)
-  	    	echo "Invalid argument. Give correct arguments"
-	    	help=true
-	    	;;
+        *)
+            echo "Invalid argument. Give correct arguments"
+	    help=true
+	    ;;
     esac
 done
 
@@ -44,13 +44,18 @@ if ! $help; then
     run_command systemctl start bluetooth.service
     run_command systemctl daemon-reload
 
-# Check if the alias already exists in .bashrc
-if ! grep -q "alias blueman=" ~/.bashrc; then
-    echo "Adding Blueman alias to .bashrc..."
-    echo "alias blueman='blueman-manager'" >> ~/.bashrc
-    source ~/.bashrc
+    # Check if the alias already exists in .bashrc
+    if ! grep -q "alias blueman=" ~/.bashrc; then
+        echo "Adding Blueman alias to .bashrc..."
+	echo "alias blueman='blueman-manager'" >> ~/.bashrc
+        source ~/.bashrc
+    else
+        echo "Blueman alias already exists in .bashrc. Skipping addition."
+    fi
 else
-    echo "Blueman alias already exists in .bashrc. Skipping addition."
+    echo "Options:"
+    echo " -r, --root       run script with sudo"
+    echo " -h, --help       display this help message"
 fi
 
 read -p "Would you like to reboot now? [y/N]: " reboot_choice

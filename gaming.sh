770aecc52bf186889a192bf5d8c39f3b7748a55c
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2024-12-07 19:48:26 +0000

    Update gaming.sh

--- gaming.sh
+++ gaming.sh
@@ -4,64 +4,44 @@ set -e
 
 # Variables
 user=$(whoami)
-help=false
 
-# Parse arguments
-for arg in "$@"; do
-    case $arg in
-	-h | --help)
-            help=true
-            ;;
-	*)
-  	    echo "Invalid argument. Give correct arguments"
-            help=true
-	    ;;
-    esac
-done
-
-if ! $help; then
-    # Check if yay is installed
-    if ! command -v yay &> /dev/null; then
-        echo "yay is not installed. Please install yay first."
-        read -p "Would you like to install yay now? [y/N]: " yay_choice
-        case $yay_choice in
-            y | Y)
-                # Install yay
-                git clone https://aur.archlinux.org/yay-bin.git
-                sudo chown "$user:$user" -R yay-bin && cd yay-bin
-                makepkg -si && cd .. && rm -rf yay
-                ;;
-            *)
-                echo "Exiting. Please install yay to proceed."
-                exit 1
-                ;;
-        esac
-    fi
-    
-    # Install required packages
-    yay -S steam wine flatpak winetricks
-    
-    # Prompt the user to install Roblox
-    read -p "Do you want to install Roblox? [y/N]: " choice
-    case $choice in
+if ! command -v yay &> /dev/null; then
+    echo "yay is not installed. Please install yay first."
+    read -p "Would you like to install yay now? [y/N]: " yay_choice
+    case $yay_choice in
         y | Y)
-            flatpak install --user https://sober.vinegarhq.org/sober.flatpakref
-            ;;
+            # Install yay
+            git clone https://aur.archlinux.org/yay-bin.git
+            sudo chown "$user:$user" -R yay-bin && cd yay-bin
+            makepkg -si && cd .. && rm -rf yay
+           ;;
         *)
-            echo "Roblox installation skipped."
-            ;;
+           echo "Exiting. Please install yay to proceed."
+           exit 1
+           ;;
     esac
+fi
     
-    # Check if the alias already exists in .bashrc
-    if ! grep -q "alias roblox=" ~/.bashrc; then
-        echo "Adding Roblox alias to .bashrc..."
-        echo "alias roblox='flatpak run org.vinegarhq.Sober'" >> ~/.bashrc
-    else
-        echo "Roblox alias already exists in .bashrc. Skipping addition."
-    fi
+# Install required packages
+yay -S steam wine flatpak winetricks
+    
+# Prompt the user to install Roblox
+read -p "Do you want to install Roblox? [y/N]: " choice
+case $choice in
+    y | Y)
+        flatpak install --user https://sober.vinegarhq.org/sober.flatpakref
+        ;;
+    *)
+        echo "Roblox installation skipped."
+        ;;
+esac
+    
+# Check if the alias already exists in .bashrc
+if ! grep -q "alias roblox=" ~/.bashrc; then
+    echo "Adding Roblox alias to .bashrc..."
+    echo "alias roblox='flatpak run org.vinegarhq.Sober'" >> ~/.bashrc
 else
-    echo "Options:"
-    echo " -h, --help       display this help message"
+    echo "Roblox alias already exists in .bashrc. Skipping addition."
 fi
 
 # Prompt the user to reboot

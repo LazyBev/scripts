0aea45d4a282f792277f34b14bb4febcc40e064c
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2024-12-07 19:49:25 +0000

    Update microcode.sh

--- microcode.sh
+++ microcode.sh
@@ -3,16 +3,12 @@
 set -e
 
 # Variables
-root=false
 print=false
 help=false
 
 # Parse arguments
 for arg in "$@"; do
     case $arg in
-        -r | --root)
-            root=true
-            ;;
         -p | --print-cpu)
             print=true
             ;;
@@ -27,41 +23,25 @@ for arg in "$@"; do
 done
 
 if ! $help; then
-    # Check for root privileges
-    if ! $root || [[ $EUID -ne 0 ]]; then
-        echo "Please run as root or use the -r option or run this script with sudo"
-        exit 1
-    fi
-
     # Detect CPU type
     if grep -q "Intel" /proc/cpuinfo; then
         cpu_type="Intel"
     else
         cpu_type="AMD"
     fi
-    
-    # Function to execute commands
-    run_command() {
-        if $root; then
-            sudo "$@"
-        else
-            "$@"
-        fi
-    }
 
     # Doing things absed on arguments chosen
     if [[ -z $print ]]; then
         if [[ $cpu_type == "Intel" ]]; then
-            run_command pacman -Syu intel-ucode
+            sudo pacman -Syu intel-ucode
         else
-            run_command pacman -Syu amd-ucode
+            sudo pacman -Syu amd-ucode
         fi
     else
         echo "You have an $cpu_type CPU"
     fi
 else 
     echo "Options:"
-    echo " -r, --root       run script with sudo"
     echo " -p, --print-cpu  only print CPU, no installing microcodes"
     echo " -h, --help       display this help message"
 fi

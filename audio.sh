207a5604f4ff2e1c37cca0cd04b47a510a38fe7c
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2024-12-07 19:40:57 +0000

    Update audio.sh

--- audio.sh
+++ audio.sh
@@ -3,15 +3,11 @@
 set -e
 
 # Variables
-root=false
 help=false
 
 # Parse arguments
 for arg in "$@"; do
     case $arg in
-	-r | --root)
-	    root=true
-            ;;
 	-h | --help)
             help=true
             ;;
@@ -24,31 +20,14 @@ done
 
 
 if ! $help; then
-
-    # Check for root privileges
-    if ! $root || [[ $EUID -ne 0 ]]; then
-        echo "Please run as root or use the -r option or run this script with sudo"
-        exit 1
-    fi
-
-    # Function to execute commands
-    run_command() {
-        if $root; then
-            sudo "$@"
-        else
-            "$@"
-        fi
-    }
-
-    run_command pacman -Syu alsa-utils pulseaudio pulseaudio-alsa pavucontrol
-    run_command sed -i '/load-module module-suspend-on-idle/s/^/# /' /etc/pulse/default.pa
-    run_command systemctl enable pulseaudio.service && systemctl start pulseaudio.service
-    run_command bash -c 'echo "defaults.pcm.card 0" > /etc/asound.conf'
-    run_command bash -c 'echo "defaults.ctl.card 0" >> /etc/asound.conf'
+    sudo pacman -Syu alsa-utils pulseaudio pulseaudio-alsa pavucontrol
+    sudo sed -i '/load-module module-suspend-on-idle/s/^/# /' /etc/pulse/default.pa
+    sudo systemctl enable pulseaudio.service && systemctl start pulseaudio.service
+    sudo bash -c 'echo "defaults.pcm.card 0" > /etc/asound.conf'
+    sudo bash -c 'echo "defaults.ctl.card 0" >> /etc/asound.conf'
 
 else
     echo "Options:"
-    echo " -r, --root       run script with sudo"
     echo " -h, --help       display this help message"
 fi
 

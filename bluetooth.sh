76d0cce9b5e8bd4fd120c6e2597daa9479be7f57
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2024-12-07 19:39:42 +0000

    Update bluetooth.sh

--- bluetooth.sh
+++ bluetooth.sh
@@ -3,15 +3,11 @@
 set -e
 
 # Variables
-root=false
 help=false
 
 # Parse arguments
 for arg in "$@"; do
     case $arg in
-	-r | --root)
-  	    root=true
-            ;;
 	-h | --help)
             help=true
             ;;
@@ -23,26 +19,10 @@ for arg in "$@"; do
 done
 
 if ! $help; then
-
-    # Check for root privileges
-    if ! $root || [[ $EUID -ne 0 ]]; then
-        echo "Please run as root or use the -r option or run this script with sudo"
-        exit 1
-    fi
-
-    # Function to execute commands
-    run_command() {
-        if $root; then
-            sudo "$@"
-        else
-            "$@"
-        fi
-    }
-    
-    run_command pacman -S bluez bluez-utils blueman pulseaudio-bluetooth
-    run_command systemctl enable bluetooth.service
-    run_command systemctl start bluetooth.service
-    run_command systemctl daemon-reload
+    sudo pacman -S bluez bluez-utils blueman pulseaudio-bluetooth
+    sudo systemctl enable bluetooth.service
+    sudo systemctl start bluetooth.service
+    sudo systemctl daemon-reload
 
     # Check if the alias already exists in .bashrc
     if ! grep -q "alias blueman=" ~/.bashrc; then
@@ -54,7 +34,6 @@ if ! $help; then
     fi
 else
     echo "Options:"
-    echo " -r, --root       run script with sudo"
     echo " -h, --help       display this help message"
 fi
 

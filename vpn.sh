a64c87a83c56716376aec6723f0bd7a347c3e1df
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2024-12-07 19:53:16 +0000

    Delete vpn.sh

--- vpn.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-sudo pacman -S networkmanager-openvpn
-wget https://swupdate.openvpn.org/community/releases/openvpn-2.6.12.tar.gz
-tar -xvzf openvpn-* cd openvpn-*/
-./configure
-make && make install

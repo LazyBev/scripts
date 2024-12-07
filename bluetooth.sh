1fff5421e197136adcb203e6dbe8065017d68f71
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2024-12-07 19:45:03 +0000

    Update bluetooth.sh

--- bluetooth.sh
+++ bluetooth.sh
@@ -2,39 +2,18 @@
 
 set -e
 
-# Variables
-help=false
+sudo pacman -S bluez bluez-utils blueman pulseaudio-bluetooth
+sudo systemctl enable bluetooth.service
+sudo systemctl start bluetooth.service
+sudo systemctl daemon-reload
 
-# Parse arguments
-for arg in "$@"; do
-    case $arg in
-	-h | --help)
-            help=true
-            ;;
-        *)
-            echo "Invalid argument. Give correct arguments"
-	    help=true
-	    ;;
-    esac
-done
-
-if ! $help; then
-    sudo pacman -S bluez bluez-utils blueman pulseaudio-bluetooth
-    sudo systemctl enable bluetooth.service
-    sudo systemctl start bluetooth.service
-    sudo systemctl daemon-reload
-
-    # Check if the alias already exists in .bashrc
-    if ! grep -q "alias blueman=" ~/.bashrc; then
-        echo "Adding Blueman alias to .bashrc..."
-	echo "alias blueman='blueman-manager'" >> ~/.bashrc
-        source ~/.bashrc
-    else
-        echo "Blueman alias already exists in .bashrc. Skipping addition."
-    fi
+# Check if the alias already exists in .bashrc
+if ! grep -q "alias blueman=" ~/.bashrc; then
+    echo "Adding Blueman alias to .bashrc..."
+    echo "alias blueman='blueman-manager'" >> ~/.bashrc
+    source ~/.bashrc
 else
-    echo "Options:"
-    echo " -h, --help       display this help message"
+    echo "Blueman alias already exists in .bashrc. Skipping addition."
 fi
 
 read -p "Would you like to reboot now? [y/N]: " reboot_choice
